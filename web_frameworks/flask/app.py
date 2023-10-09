300766294c038dd3a664227c23f0475cee6cd40a
BrianEtemesi <etemecci@gmail.com>
2023-10-09 17:44:56 +0000

    simple flask app

---
+++ web_frameworks/flask/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+simple flask application to display hello world
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world:
+    """
+    show hello world text
+    """
+    return "Hello world"
